a71637ec9d7ceea2fef09a4ecbb769c7fe38f435
Karen Quito Leon <karenquito2004@gmail.com>
2024-11-19 20:09:49 -0500

    Cliente: listar contratos, listar autos mejorada

--- src/app/modules/user-client/page/reservation-historial/reservation-historial.component.ts
+++ src/app/modules/user-client/page/reservation-historial/reservation-historial.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderServiceComponent } from "../../../../shared/components/header-service/header-service.component";
+import { TableContractComponent } from "../../../user-admin/components/table-contract/table-contract.component";
+import { UserData } from '../../../../core/models/user';
+import { AuthStateService } from '../../../../shared/services/user/auth-state.service';
+import { UserfbService } from '../../../../shared/services/user/userfb.service';
 
 @Component({
   selector: 'app-reservation-historial',
   standalone: true,
-  imports: [],
+  imports: [HeaderServiceComponent, TableContractComponent],
   templateUrl: './reservation-historial.component.html',
   styleUrl: './reservation-historial.component.scss'
 })
-export class ReservationHistorialComponent {
+export class ReservationHistorialComponent implements OnInit {
+  user!: UserData
+
+  constructor(
+    private authService: AuthStateService,
+    private userFBService: UserfbService
+  ) { }
+
+  async ngOnInit() {
+    try {
+      const currentUserUID = await this.authService.credentialUserUID;
+      if (currentUserUID) {
+        this.userFBService.listenToUserChanges(currentUserUID);
+        this.userFBService.user$.subscribe(userData => {
+          if (userData) {
+            this.user = new UserData(currentUserUID, userData);
+          }
+        });
+      }
+    } catch (error) {
+      console.error('Error al obtener el UID o al escuchar cambios en el usuario:', error);
+    }
+  }
 
 }
